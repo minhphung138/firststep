71f7015798f7ea6b222babb05dfc871d456b32b0
minhphung138 <minh141189@gmail.com>
2019-12-17 19:13:48 -0800

    Updates

---
+++ precache-manifest.6a8038c1edcad88ee597f97975251be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50331fece4e262558a0da6c558cac570",
+    "url": "/firststep/index.html"
+  },
+  {
+    "revision": "87042c1f0fc3848825d1",
+    "url": "/firststep/static/css/main.1b32b039.chunk.css"
+  },
+  {
+    "revision": "98c5773d913e42b7d447",
+    "url": "/firststep/static/js/2.ff48693f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/firststep/static/js/2.ff48693f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87042c1f0fc3848825d1",
+    "url": "/firststep/static/js/main.1cd8b37a.chunk.js"
+  },
+  {
+    "revision": "c3316d5bd627852db2b4",
+    "url": "/firststep/static/js/runtime-main.228291f5.js"
+  }
+]);
